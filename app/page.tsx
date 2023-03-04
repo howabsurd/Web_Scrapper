01c64a4de2fad8586d15f4f7e203ea7095dc06ac
Krish Poptani <poptanikrish1@gmail.com>
2023-03-05 00:21:20 +0530

    setup already done

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import {DocumentMagnifyingGlassIcon} from "@heroicons/react/24/outline";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+            <DocumentMagnifyingGlassIcon  className="h-64 w-64 text-indigo-600/20"/>
+            <h1 className="text-3xl mt-2 text-black font-bold mb-5">Welcome to the Scrapper</h1>
+            <h2>To learn how to code from ZERO experince, join ZERO TO FULL STACK HERO</h2>
+            <h3 className="text-lg italic text-center text-black/50">
+                https://www.papareact.com
+            </h3>
+    </div>
+  )
+}
+
---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import {DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+export default function Sidebar() {
+  return (
+    <div>
+        <div>
+            <DocumentMagnifyingGlassIcon className="h-16 md:w-16 text-indigo-600" />
+        </div>
+        <ul>
+
+        </ul>
+        
+    </div>
+  )
+}
